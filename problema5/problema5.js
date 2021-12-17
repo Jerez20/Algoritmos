3538ef21f9a03d3fceb34e285a7a97d2e963a61d
Jerez20 <jjerezmartes@gmail.com>
2021-12-16 21:41:31 -0400

    #6

--- problema5/problema5.js
+++ problema5/problema5.js
@@ -1,16 +1,12 @@
-/*El banco le solicita un programa para cálculo en las libretas de ahorro. Si usted ingresa una  cantidad en 
-una libreta de ahorros, su capital ira incrementando a medidas que gana  intereses mensuales. Haga un programa 
-al cual se le digite el monto a ahorrar, la tasa de  interés, los meses de ahorro y que imprima por consola el 
-monto al final del ahorro. 
- */
-
-let cantidadDeAhorros = prompt(`Introuzca su cantidad de ahorros`)*1;
-const tasaDeIntereses = prompt(`Introuzca su taza der intereses`)*1;
- // meses de ahorros 
-let mesesDeAhorros = prompt(`introduzca los meses`)*1;
-// ganacias mensuales 
-let gananciasPorMes = cantidadDeAhorros * (tasaDeIntereses/100)
-let totalGanancias  = mesesDeAhorros * gananciasPorMes;
-let montoFinalDeAhorro = totalGanancias + cantidadDeAhorros;
-
-console.log(montoFinalDeAhorro);
+let num_1 = prompt(`Introduzca el primer numero`)*1;
+let num_2 = prompt(`Introduzca el segundo numero`)*1;
+let sum=0;
+// imprima error si son negativos
+ while(num_1 < 0 || num_2 < 0) {
+     alert("Error");
+ }
+while (num_1 > 0) {
+         sum += num_2;
+         num_1--;    
+ }
+ console.log(sum);
